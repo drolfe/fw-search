fa8c2b5aa0e462a9e742c331befac6f68cbad007
drolfe <daniel.rolfe.au@gmail.com>
2014-03-23 06:59:07 -0400

    first commit

---
+++ fwfind.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+#$testcfg = File.read("test.cfg")
+get '/' do
+erb :landing
+end
+
+post '/' do
+#open the file for reading
+getpol = File.open("test.cfg", "r")
+#read and search for fw policy blocks
+fwpolicy = getpol.read.scan(/^set\sservice.*protocol.*?\n|^set\sgroup.*address.*?\n|^set\saddress.*?\n|(?m)set\spolicy.*?exit/)
+#seach policy blocks from user imput
+$search_txt = params[:search_txt]
+@results = fwpolicy.select {|pol| pol.include? $search_txt}
+erb :table
+end
