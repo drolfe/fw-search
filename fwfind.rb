76a0f2ba92327dee18aeffa37b8fdeccdef72a1b
drolfe <daniel.rolfe.au@gmail.com>
2014-03-23 07:30:21 -0400

    small comment typo

--- fwfind.rb
+++ fwfind.rb
@@ -9,7 +9,7 @@ post '/' do
 getpol = File.open("test.cfg", "r")
 #read and search for fw policy blocks
 fwpolicy = getpol.read.scan(/^set\sservice.*protocol.*?\n|^set\sgroup.*address.*?\n|^set\saddress.*?\n|(?m)set\spolicy.*?exit/)
-#seach policy blocks from user imput
+#seach policy blocks from user input
 $search_txt = params[:search_txt]
 @results = fwpolicy.select {|pol| pol.include? $search_txt}
 erb :table
