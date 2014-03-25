c58ee1844fa9a2f77c49b05603b6b682318eefb1
drolfe <daniel.rolfe.au@gmail.com>
2014-03-25 07:25:55 -0400

    added typeahead jquery to search box

--- fwfind.rb
+++ fwfind.rb
@@ -1,16 +1,22 @@
 require 'sinatra'
 
+$getpol = File.open("test.cfg", "r")
+$fwpolicy = $getpol.read.scan(/^set\sservice.*protocol.*?\n|^set\sgroup.*address.*?\n|^set\saddress.*?\n|(?m)set\spolicy.*?exit/)
+$hint = Array.new
+$fwpolicy.each do |x|
+w = x.gsub(/"/, '').split(" ")
+$hint.push(w)
+end
+$hints = $hint.flatten.uniq.sort
 get '/' do
+
 erb :landing
 end
 
 post '/' do
-#open the file for reading
-getpol = File.open("test.cfg", "r")
-#read and search for fw policy blocks
-fwpolicy = getpol.read.scan(/^set\sservice.*protocol.*?\n|^set\sgroup.*address.*?\n|^set\saddress.*?\n|(?m)set\spolicy.*?exit/)
+
 #seach policy blocks from user input
 $search_txt = params[:search_txt]
-@results = fwpolicy.select {|pol| pol.include? $search_txt}
+@results = $fwpolicy.select {|pol| pol.include? $search_txt}
 erb :table
 end
