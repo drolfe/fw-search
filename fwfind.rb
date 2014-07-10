e5eb16a4417d8d597fa4dd519a9abefa389de4be
drolfe <daniel.rolfe.au@gmail.com>
2014-07-10 03:58:01 -0400

    changes

--- fwfind.rb
+++ fwfind.rb
@@ -16,7 +16,6 @@ end
 post '/' do
 
 #seach policy blocks from user input
-$search_txt = nil
 $search_txt = params[:search_txt]
 @results = $fwpolicy.select {|pol| pol.include? $search_txt}
 erb :table
